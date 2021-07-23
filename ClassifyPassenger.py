e77f45d9d4df2dbaf06d8374973b1edadc6e5ac5
Avery Chan <JustinAveryChan@gmail.com>
2021-07-22 20:31:00 -0500

    starting 1

--- ClassifyPassenger.py
+++ ClassifyPassenger.py
@@ -1,5 +1,13 @@
 __author__ = "Avery Chan and Sudeep Reddy"
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
 print("Classification starting...")
+
+train_data = pd.read_csv("./titanic/train.csv")
+
+# print(train_data)
+# men = train_data.loc[train_data.Sex == 'male']["Survived"]
 
 print("Classification end.")
