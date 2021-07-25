3a9ad48fdac2fe839df6b48ab2020cce792f9723
Avery Chan <JustinAveryChan@gmail.com>
2021-07-25 14:51:58 -0500

    feat: added imputation

--- ClassifyPassenger.py
+++ ClassifyPassenger.py
@@ -2,11 +2,16 @@ __author__ = "Avery Chan and Sudeep Reddy"
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer
 
 print("Classification starting...")
 
 train_data = pd.read_csv("./titanic/train.csv")
+# train_data_no_na = pd.read_csv("./titanic/train_no_na.csv")
 test_data = pd.read_csv("./titanic/test.csv")
+# test_data_no_na = pd.read_csv("./titanic/test_no_na.csv")
+# print(train_data_no_na)
+# print(test_data_no_na)
 print(train_data)
 print(test_data)
 
@@ -17,13 +22,13 @@ features = [
     "Pclass",
     # "Name",
     "Sex",
-    # "Age",
+    "Age",
     "SibSp",
     "Parch",
     # "Ticket",
-    # "Fare", # this has a single NA
+    "Fare", # this has a single NA
     # "Cabin",
-    # "Embarked"
+    "Embarked"
 ]
 
 # Cleans data really nicely - seperates into numbers where applicable
@@ -32,9 +37,14 @@ X_test = pd.get_dummies(test_data[features])
 print(X)
 print(X_test)
 
-model = RandomForestClassifier(n_estimators=1000, max_depth=8, random_state=1)
-model.fit(X, y)
-predictions = model.predict(X_test)
+imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+imp = imp.fit(X)
+X_train_imp = imp.transform(X)
+X_test_imp = imp.transform(X_test)
+
+model = RandomForestClassifier(n_estimators=10000, max_depth=15, random_state=1)
+model.fit(X_train_imp, y)
+predictions = model.predict(X_test_imp)
 output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
 output.to_csv('my_submission.csv', index=False)
 
